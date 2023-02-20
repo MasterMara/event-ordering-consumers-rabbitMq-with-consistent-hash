06fcd5eb8f45aa36e2c9ab7b651cd8ab2432d4ab
Master Mara <mustafa.karacabey.engineerx@gmail.com>
2023-02-20 15:26:50 +0300

    [Continue to Implement Consistent Hash Exchange]

--- pkg/constant/constant.go
+++ pkg/constant/constant.go
@@ -2,12 +2,12 @@ package constant
 
 // QueueName Rabbit Config Const
 const QueueName = "Order.Projection"
-const RoutingKey = "Order.*.*"
+const RoutingKey = "Order.Master.İstanbul"
 
 // OrderLineCreatedExchange Const
-const OrderLineCreatedExchange = "Order.Events.V1.OrderLine:Created"
-const OrderLineInProgressedExchange = "Order.Events.V1.OrderLine:InProgressed"
-const OrderLineInTransittedExchange = "Order.Events.V1.OrderLine:InTransitted"
-const OrderLineDeliveredExchange = "Order.Events.V1.OrderLine:Delivered"
+const OrderLineCreatedExchange = "Order.Events.V1.Order:Created"
+const OrderLineInProgressedExchange = "Order.Events.V1.Order:InProgressed"
+const OrderLineInTransittedExchange = "Order.Events.V1.Order:InTransitted"
+const OrderLineDeliveredExchange = "Order.Events.V1.Order:Delivered"
 
 const qaConfigPath = "C:\\Users\\musta\\OneDrive\\Masaüstü\\event-ordering-consumers-rabbitMq-with-consistent-hash\\tools\\k8s-files\\qa\\config.qa.json"
